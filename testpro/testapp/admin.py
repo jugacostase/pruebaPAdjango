0d27c2b3787a6ebf17fded1ed70e1de42ebeeee2
Juan Guillermo <jugacostase@unal.edu.co>
2018-11-11 02:19:37 -0500

    con db

--- testpro/testapp/admin.py
+++ testpro/testapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Persona, Reporte, Tarea
 
-# Register your models here.
+admin.site.register(Persona)
+admin.site.register(Reporte)
+admin.site.register(Tarea)
